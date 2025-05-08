7940a66b8b640df00e34995e22a8eeb72f6ead61
yondervsqz <Yonder.v16@icloud.com>
2025-05-08 09:59:22 -0600

    Login Page

---
+++ cypress/e2e/pages/login/login.data.js
@@ -0,0 +1,8 @@
+export class LoginData{
+    static get credencialesInvalidas(){
+        return {
+            username: "abc",
+            password: "abc"
+        };
+    }
+}
---
+++ cypress/e2e/pages/login/login.metodos.js
@@ -0,0 +1,23 @@
+import { LoginElements } from "./login.elements";
+
+export class LoginMetodos{
+    static insertUsername(username){
+        LoginElements.textboxes.username.type(username)
+    }
+
+    static insertPassword(password){
+        LoginElements.textboxes.password.type(password)
+    }
+
+    static clickOnLoginButton(){
+        LoginElements.buttons.login.click()
+    }
+
+    static login(username, password){
+        this.insertUsername(username)
+        this.insertPassword(password)
+        this.clickOnLoginButton()
+    }
+
+
+}
