dcb75ead6100b4f3af4bcbfbd9143c0e3f7a9122
yondervsqz <Yonder.v16@icloud.com>
2025-05-08 10:21:22 -0600

    Fix report and flakyness

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  retries: 1,
   e2e: {
     setupNodeEvents(on, config) {
             allureWriter(on, config);
-            return config;
+            return config; 
     },
   },
 });
