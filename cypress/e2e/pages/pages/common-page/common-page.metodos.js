c1d71fad8bc95c9c97e358ab5ce625a8e9fce132
yondervsqz <Yonder.v16@icloud.com>
2025-05-12 10:12:12 -0600

    Creacion de pagina Common page

---
+++ cypress/e2e/pages/pages/common-page/common-page.metodos.js
@@ -0,0 +1,34 @@
+import { CommonPageData } from "./common-page.data";
+import { CommonPageElements } from "./common-page.elements";
+
+export class CommonPageMetodos{
+
+    static navigateToDemoBlaze(){
+        cy.visit(CommonPageData.url); 
+    }
+
+    static clickOnHomeOption(){
+        CommonPageElements.topMenu.home.click();
+    }
+
+    static clickOnContactOption(){
+        CommonPageElements.topMenu.contact.click();
+    }
+
+    static clickOnAboutusOption(){
+        CommonPageElements.topMenu.aboutUs.click();
+    }
+
+    static clickOnCartOption(){
+        CommonPageElements.topMenu.cart.click();
+    }
+
+    static clickOnLoginOption(){
+        CommonPageElements.topMenu.login.click();
+    }
+
+    static clickOnSignupOption(){
+        CommonPageElements.topMenu.signup.click();
+    }
+
+}
