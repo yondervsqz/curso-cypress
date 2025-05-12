c01e2df38d6d57753ecee6b5920f9c29cb308542
yondervsqz <Yonder.v16@icloud.com>
2025-05-12 08:56:49 -0600

    Creacion de la pagina Place Order

---
+++ cypress/e2e/pages/pages/place-order/place-order.metodos.js
@@ -0,0 +1,37 @@
+import { PlaceOrderElements } from "./place-order.elements";
+
+export class PlaceOrderMetodos{
+
+    static insertName(name){
+        PlaceOrderElements.textboxes.name.invoke('val', name);
+    }
+
+    static insertCountry(country){
+        PlaceOrderElements.textboxes.country.invoke('val', country);
+    }
+
+    static insertCity(city){
+        PlaceOrderElements.textboxes.city.invoke('val', city);
+    }
+
+    static insertCreditCard(creditcard){
+        PlaceOrderElements.textboxes.creditCard.invoke('val', creditcard);
+    }
+
+    static insertMonth(month){
+        PlaceOrderElements.textboxes.month.invoke('val', month);
+    }
+
+    static insertYear(year){
+        PlaceOrderElements.textboxes.year.invoke('val', year);
+    }
+
+    static clickOnCloseButton(){
+        PlaceOrderElements.buttons.close.click();
+    }
+
+    static clickOnPurchaseButton(){
+        PlaceOrderElements.buttons.purchase.click();
+    }
+
+}
