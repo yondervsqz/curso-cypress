02176a6c8740a03971a9e5b5629a483042e5a834
yondervsqz <Yonder.v16@icloud.com>
2025-05-12 08:01:00 -0600

    Creacion de archivos del Home page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
+import { HomeMetodos } from "./pages/pages/home/home.metodos"
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://demoblaze.com/index.html')
+    HomeMetodos.clickOnProductLink("Iphone 6 32gb")
+    cy.wait(5000)
   })
 })
