3789d31ccd3d6f78e87f3a307813188df98a8415
yondervsqz <Yonder.v16@icloud.com>
2025-05-13 08:30:40 -0600

    Crear usuario aleatorio

--- cypress/e2e/pages/pages/common-page/common-page.metodos.js
+++ cypress/e2e/pages/pages/common-page/common-page.metodos.js
@@ -38,4 +38,21 @@ export class CommonPageMetodos{
         })
     }
 
+    static generateRandomString(lenght=10){
+
+        let result = "";
+        const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        const charactersLenght = characters.length;
+
+        let counter = 0;
+        while (counter < lenght) {
+            result = characters.charAt(Math.floor(Math.random() * charactersLenght));
+            counter += 1;
+        }
+        return result;
+
+    }
+
+
+
 }
--- cypress/e2e/pages/pages/tests/registro-y-autenticacion.cy.js
+++ cypress/e2e/pages/pages/tests/registro-y-autenticacion.cy.js
@@ -3,6 +3,9 @@ import { CommonPageMetodos } from "../common-page/common-page.metodos";
 import { SignupMetodos } from "../signUp/signup.metodos";
 import { Logger } from "../util/logger";
 
+const user = CommonPageMetodos.generateRandomString();
+const password = CommonPageMetodos.generateRandomString(7);
+
 describe(CommonPageData.testSuites.registroYAutenticacion, ()=> {
     it("Registro de usuario válido", ()=> {
         Logger.stepNumber(1)
@@ -15,13 +18,13 @@ describe(CommonPageData.testSuites.registroYAutenticacion, ()=> {
 
         Logger.stepNumber(3)
         Logger.step("Completar los compos obligatorios con la información válida")
-        SignupMetodos.insertUsername("sajhdakhdshda")
-        SignupMetodos.insertPassword("sajhdakhdshda")
+        SignupMetodos.insertUsername(user)
+        SignupMetodos.insertPassword(password)
 
         Logger.stepNumber(4)
         Logger.step("Hacer click on Sign up para registrar al usuario")
         SignupMetodos.clickOnSignupButton();
-        CSSSkewY.wait(3000) 
+        cy.wait(3000) 
         Logger.verification("Verificacion que se muestre el mensaje 'Sign up successful");
         SignupMetodos.verifySignupSuccessfulMessageIsDisplayed();
 
